df27df80b9ba8a6b26d5b0ed2835ff41e4569122
Stuart Huddy <program.foo@gmail.com>
2016-11-19 15:41:34 +0000

    Added the main class

---
+++ Snake4.java
@@ -0,0 +1,21 @@
+/**
+ * Dundee university Quakathon
+ *
+ **/
+
+public class Snake4{
+    //Global variables
+
+    /*Constructor
+     *
+     */
+    public Snake4(){
+
+    }
+
+
+    public static void main(){
+        System.out.println("Welcome to Snake4!");
+    }
+
+}
