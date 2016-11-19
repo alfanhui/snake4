4f7afa7600a8c4e55d1a57852b07f16d71e2f628
Stuart Huddy <program.foo@gmail.com>
2016-11-19 16:09:51 +0000

    test again

--- Snake4.java
+++ Snake4.java
@@ -2,32 +2,30 @@
  * Dundee university Quackathon
  *
  **/
-//Global variables
-public int map[][];
-public int location[][];
-public int head[][], int tail[][];
-public int current_position[][];
 
 
 
 
 public class Snake4{
+    //Global variables
+    public int map[][];
+    public int location[][];
+    public int head[][];
+    public int tail[][];
+    public int current_position[][];
 
     /*Constructor
      *
      */
     public Snake4(){
-        map = new int[][];
+        map = new int[60][60];
 
     }
 
-<<<<<<< HEAD
-=======
-	public void move() {
-		
-	}
+    public void move() {
 
->>>>>>> 40d89d50c75e07756dee2a6da555df03a3d24fac
+    }
+
     public static void main(String args[]){
         System.out.println("Welcome to Snake4!");
         //game loop
