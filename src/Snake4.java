a1e613075cdaad64591cb765740ff62354f9678c
Nnerlie <vkpursiainen@gmail.com>
2016-11-20 10:10:02 +0000

    Food should appear now

--- src/Snake4.java
+++ src/Snake4.java
@@ -163,27 +163,27 @@ public class Snake4{
     public int throughWall(int[] parts) {
         switch(direction) {
             case 'l':
-                for (int i=parts[0]; i>width; i++) {
-                    if (map[i][parts[1]] == 1) {
-                        return convertToInt(i,parts[1]);
+                for (int i=parts[1]; i>width; i++) {
+                    if (map[parts[0]][i] == 1) {
+                        return convertToInt(parts[0],i);
                     }
                 }
             case 'r':
-                for (int i = parts[0]; i<=0; i--) {
-                    if (map[i][parts[1]] == 1) {
-                        return convertToInt(i,parts[1]);
-                    }
-                }
-            case 'u':
                 for (int i = parts[1]; i<=0; i--) {
                     if (map[parts[0]][i] == 1) {
                         return convertToInt(parts[0],i);
                     }
                 }
+            case 'u':
+                for (int i = parts[0]; i<=0; i--) {
+                    if (map[i][parts[1]] == 1) {
+                        return convertToInt(i,parts[1]);
+                    }
+                }
             case 'd':
-                for (int i = parts[1]; i>height; i++) {
-                    if (map[parts[0]][i] == 1) {
-                        return convertToInt(parts[0],i);
+                for (int i = parts[0]; i>height; i++) {
+                    if (map[i][parts[1]] == 1) {
+                        return convertToInt(i,parts[1]);
                     }
                 }
             default:
